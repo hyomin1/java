589e77ccc64d82f4020a9afbf0d536b60d26de30
hyomin <leehm123456@gmail.com>
2022-07-15 15:20:39 +0900

    명품 자바 3-5

--- unit3/src/unit3/ch3_4.java
+++ unit3/src/unit3/ch3_4.java
@@ -4,16 +4,19 @@ public class ch3_4 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.print("소문자 알파벳 하나를 입력하시오>>");
-		String s = scan.next();
-		char word = s.charAt(0);
-		for(char c= word;c>='a';c--) {
-			for(char d = 'a';d<=c;d++) 
-				System.out.print(d);
-			System.out.println();
-		}
+		int number[] = new int[10];
+		System.out.print("양의 정수 10개를 입력하시오>>");
+		for(int i=0;i<number.length;i++) {
+			number[i]=scan.nextInt();}
+		System.out.print("3의 배수는 ");
+		for(int i=0;i<number.length;i++)
+			if(number[i]%3==0)
+				System.out.print(number[i]+" ");
+		scan.close();
+			
 		
 
-	}
+	
 
+}
 }
