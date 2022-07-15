1218e8f6e7c6772795f0e5f8e91b918ddd3ef557
hyomin <leehm123456@gmail.com>
2022-07-15 15:24:45 +0900

    명품 자바 3-5

---
+++ unit3/src/unit3/ch3_5.java
@@ -0,0 +1,21 @@
+package unit3;
+
+import java.util.Scanner;
+
+public class ch3_5 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int number[] = new int[10];
+		System.out.print("양의 정수 10개를 입력하시오>>");
+		for(int i=0;i<number.length;i++) {
+			number[i]=scan.nextInt();}
+		System.out.print("3의 배수는");
+		for(int i=0;i<number.length;i++)
+			if(number[i]%3==0)
+				System.out.print(number[i]+" ");
+		scan.close();
+
+	}
+
+}
