65c2a2cc05c9a9cc8fb0a281430396e0b03cb0d1
hyomin <leehm123456@gmail.com>
2022-07-20 16:32:26 +0900

    명품 자바 4-2

---
+++ unit4/src/unit4/ch4_2.java
@@ -0,0 +1,27 @@
+package unit4;
+import java.util.Scanner;
+class Grade {
+	int math,science,english;
+	public Grade() {
+	}
+	public Grade(int math,int science,int english) {
+		this.math=math;
+		this.science=science;
+		this.english=english;
+	}
+	public double average() {
+		return (math+science+english) /3;
+	}
+}
+public class ch4_2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("수학, 과학, 영어 순으로 3개의 점수 입력>>");
+		int math = scanner.nextInt();
+		int science = scanner.nextInt();
+		int english = scanner.nextInt();
+		Grade me = new Grade(math, science,english);
+		System.out.println("평균은 "+me.average());
+		scanner.close();	
+	}
+}
