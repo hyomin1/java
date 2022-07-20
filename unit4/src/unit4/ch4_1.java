13c2f0da3bc2275e1e6f0dedff462f47c67169cf
hyomin <leehm123456@gmail.com>
2022-07-20 16:16:05 +0900

    명품 자바 4-1

---
+++ unit4/src/unit4/ch4_1.java
@@ -0,0 +1,31 @@
+package unit4;
+import java.util.Scanner;
+class TV {
+	String company;
+	int year;
+	int inch;
+	public TV() {
+		
+	}
+	public TV(String company,int year, int inch) {
+		this.company=company;
+		this.year=year;
+		this.inch=inch;
+	}
+	void show() {
+		System.out.print(company+"에서 만든 "+year+"년형 " +inch+"인치 TV");
+	}
+	
+	
+}
+public class ch4_1 {
+	
+	public static void main(String[] args) {
+		
+		TV myTV = new TV("LG",2017,32);
+		myTV.show();
+		
+	}
+}
+
+
